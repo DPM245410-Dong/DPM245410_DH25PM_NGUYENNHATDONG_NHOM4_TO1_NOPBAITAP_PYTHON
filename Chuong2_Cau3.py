ca3b9c18774209634daa7461cde8e1c5abb7176a
nguyennhdong <nguyennhdong@gmail.com>
2025-09-18 10:19:29 +0700

    Nộp bài tập chương 2

---
+++ Chuong2_Cau3.py
@@ -0,0 +1,6 @@
+toan=float(input("Nhập điểm toán:"))
+ly=float(input("Nhập điểm lý:"))
+hoa=float(input("Nhập diểm hóa:"))
+dtb = (toan+ly+hoa)/3
+print("Điểm trung bình=", dtb)
+print("Điểm làm tròn=",round(dtb,2))
